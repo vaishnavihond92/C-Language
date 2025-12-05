607c0c1b7da8427c35bbe2311ba24320bf2408a5
vaishnavihond92 <vaishnavihond@gmail.com>
2025-12-05 19:57:16 +0530

    Add files via upload

---
+++ reverse_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+   int num,d,rev=0;
+   printf("enter number :");
+   scanf("%d",&num);
+   
+   while(num>0)
+     {
+        d=num%10;
+        rev=rev*10+d;
+        num=num/10;
+     }
+     printf("Reverse number=%d",rev);
+}

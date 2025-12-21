a57ab2ea8620559169777476d8d7b57cce20775e
vaishnavihond92 <vaishnavihond@gmail.com>
2025-12-21 22:03:53 +0530

    Add files via upload

---
+++ Arithmetic_operators.c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a=45,b=50;
+	printf("\n Addition=%d",a+b);
+	printf("\n Subtraction=%d",a-b);
+	printf("\n Multiplication=%d",a*b);
+	printf("\n Division=%d",a/b);
+	printf("\n Mod=%d",a%b);
+}

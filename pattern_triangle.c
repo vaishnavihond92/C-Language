98a83c0c138fac7b8b0dcf19d162be7e9c2f9e8e
vaishnavihond92 <vaishnavihond@gmail.com>
2026-02-03 21:19:53 +0530

    Add files via upload

---
+++ pattern_triangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,a=1;
+	printf("Enter limit :");
+	scanf("%d",&n);
+	for(i=1; i<=n; i++)
+	{
+		for(j=1; j<=i; j++)
+		{
+			printf(" %d",a);
+			a++;
+		}
+		printf("\n");
+	}
+
+}

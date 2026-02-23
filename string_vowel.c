9e3110de59c167d54de93b974e240b07382b8e3d
vaishnavihond92 <vaishnavihond@gmail.com>
2026-02-23 21:49:22 +0530

    Add files via upload

---
+++ string_vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i,cnt=0;
+	char s1[30];
+	printf("Enter String :");
+	gets(s1);
+	for(i=0; s1[i]>'\0'; i++)
+	{
+		if(s1[i]=='A'|| s1[i]=='a'|| s1[i]=='E'|| s1[i]=='e'|| 
+		s1[i]=='I'|| s1[i]=='i'|| s1[i]=='O'|| s1[i]=='o'|| s1[i]=='U'|| s1[i]=='u')
+		{
+			cnt++;
+		}
+	}
+	printf("Vowel=%d",cnt);
+}
